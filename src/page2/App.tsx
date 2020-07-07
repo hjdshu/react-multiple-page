5a491e9628d5993432bbf513b3ded5fb07e6f582
hejia <jia.he@yottacloud.cn>
2020-07-07 18:03:36 +0800

    update-use

--- src/page2/App.tsx
+++ src/page2/App.tsx
@@ -3,13 +3,31 @@ import './App.css';
 import C1 from './components/c1';
 
 import { CounterProvider, useCounterContext } from './reduce'
+import useGetMousemove from './hooks/useMouseover'
 
 function App() {
+
+  const [y, setY] = useState(true)
+
+  const MouseMove = function () {
+    const {x, refX} = useGetMousemove()
+    return <div>x: {x}, refX: {refX}</div> 
+  }
+
+  const MouseMoveB = function () {
+    const {x, refX} = useGetMousemove()
+  return <div>B: {x}, refX: {refX}</div> 
+  }
 
   const Div = function () {
     const { count, increment } = useCounterContext()
     const incrementFunc = () => {
       increment(2)
+      if (count == 6) {
+        setY(true)
+      } else {
+        setY(false)
+      }
     }
     return (
     <button onClick={incrementFunc}>count: {count}</button>
@@ -19,8 +37,14 @@ function App() {
   return (
     <CounterProvider>
       <div className='App'>
+        { 
+          y ? <MouseMove/> : null
+        }
         <Div></Div>
         <C1></C1>
+        {
+          y ? null : <MouseMoveB/>
+        }
       </div>
     </CounterProvider>
   );
---
+++ src/page2/hooks/useMouseover.ts
@@ -0,0 +1,19 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+export default function useGetMousemove () {
+  const [x, setX] = useState(0)
+  const refX = useRef(0)
+
+  useEffect(() => {
+    const func = function (e:any) {
+      console.log('move', e.clientX)
+      setX((x) => (x+1))
+      refX.current = refX.current += 1
+    }
+    window.addEventListener('mousemove', func, false)
+    return () => {
+      window.removeEventListener('mousemove', func, false)
+    }
+  }, [])
+  return {x, refX: refX.current}
+}
