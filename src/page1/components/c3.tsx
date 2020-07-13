41f160b835434a8a528daccc82bde1b6fa7b2c9f
hejia <jia.he@yottacloud.cn>
2020-07-13 14:38:09 +0800

    m

--- src/page1/components/c3.tsx
+++ src/page1/components/c3.tsx
@@ -9,49 +9,49 @@ interface IState {
   count2: number
 }
 
-// export default function (props: IProps) {
+export default function (props: IProps) {
 
-//   // const {state, dispatch} = useContext(ThemeContext)
+  // const {state, dispatch} = useContext(ThemeContext)
 
-//   const { count } = props
+  const { count } = props
 
-//   console.log(count)
+  console.log(count)
 
-//   return (
-//     <div>
-//       <h2>this is c3</h2>
-//       count: {count}
-//       <br/>
-//     </div>
-//   )
-// }
-
-
-
-export default class C3 extends React.Component<IProps, IState>{
-
-  constructor (props: Readonly<IProps>) {
-    super(props);
-  }
-
-  shouldComponentUpdate (nextProps: IProps) {
-    if (nextProps.count != this.props.count) return true
-    return false
-  }
-
-  render () {
-    const count = this.props.count
-    console.log(count)
-    return (
-      <div>
-        <h2>this is c3</h2>
-        count: {count};
-        {/* <button onClick={() => {
-          this.addCount2()
-        }}>add count2</button>
-        count2: {count2} */}
-        <br/>
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h2>this is c3</h2>
+      count: {count}
+      <br/>
+    </div>
+  )
 }
+
+
+
+// export default class C3 extends React.Component<IProps, IState>{
+
+//   constructor (props: Readonly<IProps>) {
+//     super(props);
+//   }
+
+//   shouldComponentUpdate (nextProps: IProps) {
+//     if (nextProps.count != this.props.count) return true
+//     return false
+//   }
+
+//   render () {
+//     const count = this.props.count
+//     console.log(count)
+//     return (
+//       <div>
+//         <h2>this is c3</h2>
+//         count: {count};
+//         {/* <button onClick={() => {
+//           this.addCount2()
+//         }}>add count2</button>
+//         count2: {count2} */}
+//         <br/>
+//       </div>
+//     )
+//   }
+// }
